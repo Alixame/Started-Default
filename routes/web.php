49fae52408c93e8b69b48a18ac44ba0671f082f5
Lucas Alixame <lucasali2003@gmail.com>
2022-03-01 13:13:37 -0300

    [REF] Organizando logica das Rotas

--- routes/web.php
+++ routes/web.php
@@ -14,28 +14,27 @@ Route::get('/', function () {
 })->name('site.index');
 
 /**
- * ROTA DO TIPO [GET]
- * 
- * RETORNA VIEW DE LOGIN ADMINISTRATIVO DO SISTEMA
- * 
- * @return view;
- */
-Route::get('/admin/login', 'LoginController@viewLogin')->name('admin.login');
-
-/**
- * ROTA DO TIPO [GET]
- * 
- * RETORNA VIEW DO PAINEL DE ADMINISTRAÇÃO
- * 
- * @return view;
- */
-Route::get('/admin', 'PainelController@viewPainel')->name('admin.painel');;
-
-
-/**
  * AGRUPAMENTO DE ROTAS DO PAINEL DE ADMINISTRAÇÃO (TODAS AS ROTAS VÃO USAR O MESMO PREFIXO)
  */
 Route::prefix("/admin")->group( function(){
+
+    /**
+     * ROTA DO TIPO [GET]
+     * 
+     * RETORNA VIEW DO PAINEL DE ADMINISTRAÇÃO
+     * 
+     * @return view;
+     */
+    Route::get('', 'PainelController@viewPainel')->name('admin.painel');
+
+    /**
+     * ROTA DO TIPO [GET]
+     * 
+     * RETORNA VIEW DE LOGIN ADMINISTRATIVO DO SISTEMA
+     * 
+     * @return view;
+     */
+    Route::get('/login', 'LoginController@viewLogin')->name('admin.login');
 
 
 });
